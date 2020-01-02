a71b9f69d38d35c12015fbe4500e7ccace8622c1
japgroevemaker <joepgravemaker@gmail.com>
2020-01-02 20:32:48 +0100

    made .env file

--- server/router/api/Pages.js
+++ server/router/api/Pages.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const mongodb = require('mongodb');
-require('dotenv/config');
+require('dotenv/config')
 const router = express.Router();
 
 router.get('/:title', async function(req, res){
   const title = req.params.title
   const page = await loadPages();
-  
+
   res.send(await page.find({title:title}).toArray())
 })
 
@@ -18,8 +18,8 @@ router.get('/:title', async function(req, res){
 // })
 
 async function loadPages(){
-  const client = await mongodb.MongoClient.connect('your db', {
-    useNewUrlParser: true,
+  const client = await mongodb.MongoClient.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@intern-6gs0a.azure.mongodb.net/intern_report?retryWrites=true&w=majority`, {
+    useNewUrlParser: true,                        
     useUnifiedTopology: true
   });
   return client.db('intern_report').collection('pages')
